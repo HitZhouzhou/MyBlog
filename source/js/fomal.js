6887ecb877fd75ec36659d87fb8c9c676c70d009
zhouzhou <hitzhoujy@163.com>
2023-10-27 10:15:40 +0800

    一二挂件

--- source/js/fomal.js
+++ source/js/fomal.js
@@ -674,7 +674,7 @@ if (document.body.clientWidth > 992) {
         zoom: 0.9,
         borderRadius: 5 + 'px',
         right: 55.6 + 'px',
-        nekoImg: "https://bu.dusays.com/2022/07/20/62d812db74be9.png",
+        nekoImg: "https://hitszzhou.oss-cn-shenzhen.aliyuncs.com/assets/guayier.jpeg",
         hoverMsg: "春天啦~",
         color: "var(--theme-color)",
         during: 500,
